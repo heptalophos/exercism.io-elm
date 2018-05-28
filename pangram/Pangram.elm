94bbe367073ba018b316d4a27a84fd217bd9435b
Zero Delta <zerodeltahedge@gmail.com>
2018-05-28 18:57:21 +0300

    added pangram

---
+++ pangram/Pangram.elm
@@ -0,0 +1,8 @@
+module Pangram exposing (isPangram)
+
+import String
+
+isPangram : String -> Bool
+isPangram sentence = List.all (\letter -> String.contains letter (sentence |> String.toLower)) 
+                              ("abcdefghijklmnopqrstuvwxyz" |> String.split "")
+   
