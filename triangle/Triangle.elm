fc2115eaa0a83218b5a3a6d1c0bdadbd05ef43d3
Zero Delta <zerodeltahedge@gmail.com>
2018-06-02 00:42:26 +0300

    triangle & fixed tests

--- triangle/Triangle.elm
+++ triangle/Triangle.elm
@@ -1,8 +1,10 @@
 module Triangle exposing (Triangle(..), triangleKind, version)
 
+import Set
 
 type Triangle
-    = Equilateral
+    = Degenerate
+    | Equilateral
     | Isosceles
     | Scalene
 
@@ -14,4 +16,16 @@ version =
 
 triangleKind : comparable -> comparable -> comparable -> Result String Triangle
 triangleKind x y z =
-    Debug.crash "Please implement this function"
+        if (x <= 0 || y <= 0 || z <= 0) then
+            Result.Err "Invalid side"
+        else
+        if (x + y < z || x + z < y || y + z < x) then
+            Result.Err "Violated Triangle Inequality"
+        else 
+            if (x + y == z || x + z == y || y + z == x) then
+            Result.Ok Degenerate
+        else 
+            case Set.size(Set.fromList [x, y, z]) of
+                1 -> Result.Ok Equilateral
+                2 -> Result.Ok Isosceles
+                _ -> Result.Ok Scalene
