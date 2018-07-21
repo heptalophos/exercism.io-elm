7c56cbaf2a0f13640854637d4db1214bfe47af11
Zero Delta <zerodeltahedge@gmail.com>
2018-07-22 01:52:14 +0300

    collatz-conjecture all tests passing

--- collatz-conjecture/CollatzConjecture.elm
+++ collatz-conjecture/CollatzConjecture.elm
@@ -6,14 +6,14 @@ collatz start =
     if start <= 0 then
         Err "Only positive numbers are allowed"
     else 
-        Ok (collatzExec 0 start)
+        Ok (collatzConvergence 0 start)
 
 
-collatzExec : Int -> Int -> Int
-collatzExec steps start =
+collatzConvergence : Int -> Int -> Int
+collatzConvergence steps start =
     if start == 1 then 
         steps
     else if start % 2 == 0 then
-        collatzExec (steps + 1) (start // 2)
+        collatzConvergence (steps + 1) (start // 2)
     else 
-        collatzExec (steps + 1) (3 * start + 1)
+        collatzConvergence (steps + 1) (3 * start + 1)
