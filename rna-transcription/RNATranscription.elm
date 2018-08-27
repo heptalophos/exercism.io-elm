dacb3a7265d0b51c179085804a59e0b08c72b694
Zero Delta <zerodeltahedge@gmail.com>
2018-08-27 06:24:21 +0300

    rna-transcription all tests passing

--- rna-transcription/RNATranscription.elm
+++ rna-transcription/RNATranscription.elm
@@ -1,6 +1,20 @@
 module RNATranscription exposing (toRNA)
 
+import String exposing (cons, toList)
 
 toRNA : String -> Result Char String
 toRNA dna =
-    Debug.crash "Please implement this function"
+    let 
+        transcribe : Char -> Result Char Char
+        transcribe nuc = 
+            case nuc of
+                'G' -> Ok 'C'
+                'C' -> Ok 'G'
+                'T' -> Ok 'A'
+                'A' -> Ok 'U'
+                _   -> Err nuc
+    in
+        case String.uncons dna of
+            Just (hd, tl) -> Result.map2 cons (transcribe hd) (toRNA tl)
+            Nothing       -> Ok ""
+
