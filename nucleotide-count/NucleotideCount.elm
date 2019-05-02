32d7d8b5f1ba92e6f08818cd4c4ef798002ba7d2
Zero Delta <zerodeltahedge@gmail.com>
2019-05-03 02:04:41 +0300

    nucleotide-count solution

--- nucleotide-count/NucleotideCount.elm
+++ nucleotide-count/NucleotideCount.elm
@@ -1,10 +1,8 @@
 module NucleotideCount exposing (nucleotideCounts, version)
 
-
 version : Int
 version =
     2
-
 
 type alias NucleotideCounts =
     { a : Int
@@ -13,7 +11,17 @@ type alias NucleotideCounts =
     , g : Int
     }
 
-
 nucleotideCounts : String -> NucleotideCounts
 nucleotideCounts sequence =
-    Debug.crash "Please implement this function"
+    let 
+        inc n acc =
+            case n of 
+                'A' -> {acc | a = acc.a + 1}
+                'T' -> {acc | t = acc.t + 1}
+                'C' -> {acc | c = acc.c + 1}
+                'G' -> {acc | g = acc.g + 1}
+                _ -> acc
+    in
+        sequence
+        |> String.toList
+        |> List.foldl inc (NucleotideCounts 0 0 0 0)
