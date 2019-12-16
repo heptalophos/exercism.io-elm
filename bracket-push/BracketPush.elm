2ce6be8a2da6af21ceb9193e7bdddbe43427e056
Zero Delta <zerodeltahedge@gmail.com>
2019-12-16 18:42:33 +0200

    bracket-push solution

--- bracket-push/BracketPush.elm
+++ bracket-push/BracketPush.elm
@@ -1,6 +1,38 @@
 module BracketPush exposing (isPaired)
 
+import String exposing (contains, fromChar, toList)
+
 
 isPaired : String -> Bool
-isPaired input =
-    Debug.todo "Please implement this function"
+isPaired input = paired (toList input) []
+
+
+paired : List Char -> List Char -> Bool
+paired s1 s2 = 
+    case (s1, s2) of
+        ([], []) -> 
+            True
+        ([], _) -> 
+            False
+        ((']'::xs), ('['::ys)) -> 
+            paired xs ys
+        ((')'::xs), ('('::ys)) -> 
+            paired xs ys
+        (('}'::xs), ('{'::ys)) -> 
+            paired xs ys
+        ((x::xs), stack) -> 
+            case x of
+                '}' -> 
+                    False
+                ')' -> 
+                    False
+                ']' -> 
+                    False
+                '[' -> 
+                    paired xs (x::stack)
+                '{' -> 
+                    paired xs (x::stack)
+                '(' -> 
+                    paired xs (x::stack)
+                _   -> 
+                    paired xs stack
