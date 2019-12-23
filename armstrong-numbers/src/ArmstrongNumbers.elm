275161d4dd9b60b537adf5217f81bb73d1d72314
Zero Delta <zerodeltahedge@gmail.com>
2019-12-23 18:03:46 +0200

    Update ArmstrongNumbers.elm

--- armstrong-numbers/src/ArmstrongNumbers.elm
+++ armstrong-numbers/src/ArmstrongNumbers.elm
@@ -1,6 +1,21 @@
 module ArmstrongNumbers exposing (isArmstrongNumber)
 
+flip : (a -> b -> c) -> b -> a -> c
+flip f a b = f b a
 
 isArmstrongNumber : Int -> Bool
 isArmstrongNumber nb =
-    Debug.todo "Please implement this function"
+    let 
+        digits = 
+            nb 
+            |> String.fromInt 
+            |> String.toList 
+            |> List.map ( Maybe.withDefault 0
+                          << String.toInt
+                          << String.fromChar ) 
+        digitsCount =
+            digits |> List.length
+    in
+        digits 
+        |> List.foldl (flip (^) digitsCount >> (+)) 0 
+        |> (==) nb       
