d3b430f1108d195fbefe9aa809144104bde5c854
Zero Delta <zerodeltahedge@gmail.com>
2020-01-13 23:51:46 +0200

    Allergies.elm initial specification

--- allergies/src/Allergies.elm
+++ allergies/src/Allergies.elm
@@ -1,23 +1,22 @@
--- module Allergies exposing (Allergy(..), isAllergicTo, toList)
+module Allergies exposing (Allergy(..), isAllergicTo, toList)
 
 
--- type Allergy
---     = Eggs
---     | Peanuts
---     | Shellfish
---     | Strawberries
---     | Tomatoes
---     | Chocolate
---     | Pollen
---     | Cats
+type Allergy
+    = Eggs
+    | Peanuts
+    | Shellfish
+    | Strawberries
+    | Tomatoes
+    | Chocolate
+    | Pollen
+    | Cats
 
 
--- isAllergicTo : Allergy -> Int -> Bool
--- isAllergicTo allergy score =
---     Debug.todo "Please implement this function"
+isAllergicTo : Allergy -> Int -> Bool
+isAllergicTo allergy score =
+    Debug.todo "Please implement this function"
 
 
--- toList : Int -> List Allergy
--- toList score =
---     Debug.todo "Please implement this function"
-
+toList : Int -> List Allergy
+toList score =
+    Debug.todo "Please implement this function"
