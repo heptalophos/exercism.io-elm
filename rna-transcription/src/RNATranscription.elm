133f5bd62bade5eda3449a2f94238c112761a12b
Zero Delta <zerodeltahedge@gmail.com>
2020-03-26 16:49:15 +0200

    Create RNATranscription.elm

---
+++ rna-transcription/src/RNATranscription.elm
@@ -0,0 +1,27 @@
+module RNATranscription exposing (toRNA)
+
+import String exposing (cons, uncons, toList)
+import Result exposing (map2)
+
+toRNA : String -> Result Char String
+toRNA dna =
+    let 
+        transcribe : Char -> Result Char Char
+        transcribe nuc = 
+            case nuc of
+                'G' -> 
+                    Ok 'C'
+                'C' -> 
+                    Ok 'G'
+                'T' -> 
+                    Ok 'A'
+                'A' -> 
+                    Ok 'U'
+                _   -> 
+                    Err nuc
+    in
+        case uncons dna of
+            Just (hd, tl) -> 
+                map2 cons (transcribe hd) (toRNA tl)
+            Nothing       -> 
+                Ok ""
