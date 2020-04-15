6a36116d2738e558128bc29a922b525955d7e6b9
Zero Delta <zerodeltahedge@gmail.com>
2020-04-15 06:45:00 +0300

    Refactor gigasecond (pointfree fcn)

--- gigasecond/src/Gigasecond.elm
+++ gigasecond/src/Gigasecond.elm
@@ -3,15 +3,12 @@ module Gigasecond exposing (add)
 
 import Time 
 
-
 gigasec : Int
 gigasec = 
     10 ^ 9
 
-
 add : Time.Posix -> Time.Posix
-add toTime =
-    toTime 
-    |> Time.posixToMillis 
-       >> (+) (gigasec * 10 ^ 3)
-       >> Time.millisToPosix 
+add =
+    Time.posixToMillis 
+    >> (+) (gigasec * 10 ^ 3)
+    >> Time.millisToPosix 
