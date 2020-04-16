d5751eb03852c890b2e575f5ffbc56c97614e631
Zero Delta <zerodeltahedge@gmail.com>
2020-04-16 23:32:43 +0300

    Refactor acronym solution

---
+++ acronym/src/Acronym.elm
@@ -0,0 +1,11 @@
+module Acronym exposing (abbreviate)
+
+import List exposing (map, foldr)
+import String exposing (replace, split, slice, toUpper)
+
+abbreviate : String -> String
+abbreviate =
+    replace "-" " "
+    >> split " "
+    >> map( slice 0 1 >> toUpper )
+    >> foldr (++) ""
