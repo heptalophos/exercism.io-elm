925d329cb87b0ab39273a8e8bb39d32cd7b7e271
Zero Delta <zerodeltahedge@gmail.com>
2020-05-07 18:00:05 +0300

    two-fer solution

--- two-fer/src/TwoFer.elm
+++ two-fer/src/TwoFer.elm
@@ -1,6 +1,9 @@
 module TwoFer exposing (twoFer)
 
+import Maybe exposing ( withDefault )
+import String exposing( concat )
+
 
 twoFer : Maybe String -> String
 twoFer name =
-    Debug.todo "Please implement this function"
+    concat ["One for ", withDefault "you" name, ", one for me."]
