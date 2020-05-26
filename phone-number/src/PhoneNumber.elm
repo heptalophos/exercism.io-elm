5532aa80fe377539a280ffde1cc2da842b3ca36e
Zero Delta <zerodeltahedge@gmail.com>
2020-05-26 17:59:23 +0300

    Update PhoneNumber.elm

--- phone-number/src/PhoneNumber.elm
+++ phone-number/src/PhoneNumber.elm
@@ -14,8 +14,8 @@ getNumber : String -> Maybe String
 getNumber =
     normalize
     >> andThen (validateNumber)
-    >> andThen (validateDigit 0)
-    >> andThen (validateDigit 3)
+    >> andThen (validateDigitAt 0)
+    >> andThen (validateDigitAt 3)
 
 
 normalize : String -> Maybe String
@@ -40,9 +40,9 @@ validateNumber phoneNumber =
             Nothing
 
 
-validateDigit : Int -> String -> Maybe String
-validateDigit atIndex phoneNumber =
-    case phoneNumber |> slice atIndex (atIndex + 1) of
+validateDigitAt : Int -> String -> Maybe String
+validateDigitAt pos phoneNumber =
+    case phoneNumber |> slice pos (pos + 1) of
         "0" -> 
             Nothing
         "1" -> 
