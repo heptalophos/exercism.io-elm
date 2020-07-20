f49544f5c224a2c90483577a32052dd7195b5520
Zero Delta <zerodeltahedge@gmail.com>
2020-07-20 23:10:13 +0300

    Reformat Etl.elm

---
+++ etl/src/Etl.elm
@@ -0,0 +1,17 @@
+module Etl exposing (transform)
+
+import Dict exposing (Dict)
+
+
+transform : Dict Int (List String) -> Dict String Int
+transform input =
+    let 
+        pairs : (Int, List String) -> List (String, Int)
+        pairs (score, letters) = 
+            List.map(\letter -> 
+                        (String.toLower letter, score)) 
+            letters
+    in 
+        Dict.toList input 
+        |> List.concatMap pairs 
+        |> Dict.fromList
