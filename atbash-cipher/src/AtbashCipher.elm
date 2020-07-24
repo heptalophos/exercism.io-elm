d9928c49c22f30700f6c509fafcb1b5ff9b13b90
Zero Delta <zerodeltahedge@gmail.com>
2020-07-24 19:23:49 +0300

    Create AtbashCipher.elm

---
+++ atbash-cipher/src/AtbashCipher.elm
@@ -0,0 +1,35 @@
+module AtbashCipher exposing (decode, encode)
+
+chunkify : Int -> String -> String
+chunkify n text =
+    case text of 
+        "" -> 
+            text
+        _  -> 
+            (String.left n text) ++ 
+            " " ++ 
+            (chunkify n (String.dropLeft n text))
+
+atbash : Char -> Char 
+atbash ch =
+    if Char.isAlpha ch then
+        Char.fromCode((Char.toCode 'a') + 
+        (Char.toCode 'z') - Char.toCode ch)
+    else 
+        ch
+
+
+encode : String -> String
+encode plain = 
+    plain 
+    |> String.toLower
+    |> String.filter (\c -> Char.isLower c || Char.isDigit c)
+    |> String.map atbash
+    |> chunkify 5
+    |> String.trim
+
+
+decode : String -> String
+decode cipher = cipher
+                    |> String.filter (\c -> Char.isLower c || Char.isDigit c)
+                    |> String.map atbash
