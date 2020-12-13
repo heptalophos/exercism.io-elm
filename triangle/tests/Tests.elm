0551f1d20c316783c5d423080fa4bed26a2ba4e7
Zero Delta <zerodeltahedge@gmail.com>
2020-12-13 21:48:34 +0200

    Update Tests.elm

--- triangle/tests/Tests.elm
+++ triangle/tests/Tests.elm
@@ -10,43 +10,43 @@ tests =
     describe "triangleKind"
         [ test "equilateral triangles have equal sides" <|
             \() -> Expect.equal (Ok Equilateral) (triangleKind 2 2 2)
-        ,-- skip <|
+        , skip <|
             test "larger equilateral triangles also have equal sides" <|
                 \() -> Expect.equal (Ok Equilateral) (triangleKind 10 10 10)
-        ,-- skip <|
+        , skip <|
             test "isosceles triangles have last two sides equal" <|
                 \() -> Expect.equal (Ok Isosceles) (triangleKind 3 4 4)
-        ,-- skip <|
+        , skip <|
             test "isosceles triangles have first and last sides equal" <|
                 \() -> Expect.equal (Ok Isosceles) (triangleKind 4 3 4)
-        ,-- skip <|
+        , skip <|
             test "isosceles triangles have two first sides equal" <|
                 \() -> Expect.equal (Ok Isosceles) (triangleKind 4 4 3)
-        ,-- skip <|
+        , skip <|
             test "isosceles triangles have in fact exactly two sides equal" <|
                 \() -> Expect.equal (Ok Isosceles) (triangleKind 10 10 2)
-        ,-- skip <|
+        , skip <|
             test "scalene triangles have no equal sides" <|
                 \() -> Expect.equal (Ok Scalene) (triangleKind 3 4 5)
-        ,-- skip <|
+        , skip <|
             test "scalene triangles have no equal sides at a larger scale too" <|
                 \() -> Expect.equal (Ok Scalene) (triangleKind 10 11 12)
-        ,-- skip <|
+        , skip <|
             test "scalene triangles have no equal sides at a larger scale too 2" <|
                 \() -> Expect.equal (Ok Scalene) (triangleKind 5 4 2)
-        ,-- skip <|
+        , skip <|
             test "very small triangles are legal" <|
                 \() -> Expect.equal (Ok Scalene) (triangleKind 0.4 0.6 0.3)
-        ,-- skip <|
+        , skip <|
             test "triangles with no size are illegal" <|
                 \() -> Expect.equal (Err "Invalid lengths") (triangleKind 0 0 0)
-        ,-- skip <|
+        , skip <|
             test "triangles with negative sides are illegal" <|
                 \() -> Expect.equal (Err "Invalid lengths") (triangleKind 3 4 -5)
-        ,-- skip <|
+        , skip <|
             test "triangles violating triangle inequality are illegal 1" <|
                 \() -> Expect.equal (Err "Violates inequality") (triangleKind 1 1 3)
-        ,-- skip <|
+        , skip <|
             test "triangles violating triangle inequality are illegal 2" <|
                 \() -> Expect.equal (Err "Violates inequality") (triangleKind 7 3 2)
         ]
