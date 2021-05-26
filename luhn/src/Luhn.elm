18369c39ca2d77dfef417720a517647d56a1181f
Zero Delta <zerodeltahedge@gmail.com>
2021-05-26 21:35:33 +0300

    Update Luhn.elm

--- luhn/src/Luhn.elm
+++ luhn/src/Luhn.elm
@@ -3,4 +3,23 @@ module Luhn exposing (valid)
 
 valid : String -> Bool
 valid input =
-    Debug.todo "Please implement this function"
+    let 
+        luhn x = case x of
+            [] -> 0
+            [z] -> z
+            (x0::x1::xs) -> 
+                 x0 + 2 * x1 - (if x1 >= 5 then 9 else 0) + (luhn xs)
+        checksum = 
+            String.toList 
+            >> List.filterMap (String.fromChar >> String.toInt)
+            >> List.reverse
+            >> luhn 
+            >> (modBy 10)
+        sanitized x = if x |> String.any (Char.isDigit >> not) >> not 
+                      then "" 
+                      else x 
+    in
+        case input |> String.filter (not << (==) ' ') of
+            "" -> False
+            -- [_] -> False
+            x -> checksum x == 0            
