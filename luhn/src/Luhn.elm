2a21d59887ab0bef9809dcb72e2b52c63d49c02b
Zero Delta <zerodeltahedge@gmail.com>
2021-05-27 03:05:44 +0300

    Refactor Luhn.elm

--- luhn/src/Luhn.elm
+++ luhn/src/Luhn.elm
@@ -3,23 +3,35 @@ module Luhn exposing (valid)
 
 valid : String -> Bool
 valid input =
+
     let 
-        luhn x = case x of
-            [] -> 0
-            [z] -> z
-            (x0::x1::xs) -> 
-                 x0 + 2 * x1 - (if x1 >= 5 then 9 else 0) + (luhn xs)
+        compact = 
+            String.filter (not << (==) ' ')
+
+        isDirty = 
+            String.any (Char.isDigit >> not) 
+
+        sanitized x = 
+            if x |> compact |> isDirty then "" else x 
+
+        luhn x = 
+            case x of
+                [] -> 0
+                [z] -> z
+                (x0::x1::xs) -> 
+                    x0 + 2 * x1 - 
+                    (if x1 >= 5 then 9 else 0) + 
+                    (luhn xs)
+
         checksum = 
             String.toList 
             >> List.filterMap (String.fromChar >> String.toInt)
             >> List.reverse
             >> luhn 
-            >> (modBy 10)
-        sanitized x = if x |> String.any (Char.isDigit >> not) >> not 
-                      then "" 
-                      else x 
+            >> modBy 10
+
     in
-        case input |> String.filter (not << (==) ' ') of
-            "" -> False
-            -- [_] -> False
-            x -> checksum x == 0            
+        case input |> compact |> sanitized of
+            ""  -> False
+            "0" -> False
+            x   -> checksum x == 0            
