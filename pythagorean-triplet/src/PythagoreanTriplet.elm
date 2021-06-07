33973ce5636f3cf262805c0b9e656c2fdb489642
Zero Delta <zerodeltahedge@gmail.com>
2021-06-07 20:27:55 +0300

    Create PythagoreanTriplet.elm

---
+++ pythagorean-triplet/src/PythagoreanTriplet.elm
@@ -0,0 +1,25 @@
+module PythagoreanTriplet exposing (triplets)
+
+
+type alias Triplet =
+    ( Int, Int, Int )
+
+
+triplets : Int -> List Triplet
+triplets n = 
+    flip List.concatMap 
+         (List.range 1 (n // 2)) 
+         (\a ->
+            flip List.concatMap 
+                 (List.range (a + 1) (n // 2)) 
+                 (\b -> 
+                    let 
+                        c = n - a - b
+                    in 
+                        if a^2 + b^2 == c^2
+                        then [(a, b, c)]
+                        else []))
+
+
+flip : (a -> b -> c) -> b -> a -> c
+flip f a b = f b a
