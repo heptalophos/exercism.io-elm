7cbeb44011f93f2ad0699f18335cf86a1f0f2030
Zero Delta <zerodeltahedge@gmail.com>
2021-06-16 05:27:08 +0300

    Update Series.elm

--- series/src/Series.elm
+++ series/src/Series.elm
@@ -10,18 +10,20 @@ slices size input =
             |> List.map (String.fromChar 
                          >> String.toInt)
             |> List.map (Maybe.withDefault 0)
+            
         sublists list =
             List.range 0 (String.length input - size)
-            |> List.map ( \x -> List.drop x list 
-                                |> List.take size)
+            |> List.map (\x -> List.drop x list 
+                               |> List.take size)
     in 
         if String.isEmpty input then
-            Err "series cannot be empty"
+            Err <| "series cannot be empty"
         else if size > String.length input then
-            Err "slice length cannot be greater than series length"
+            Err <|
+            "slice length cannot be greater than series length"
         else if size == 0 then
-            Err "slice length cannot be zero"
+            Err <| "slice length cannot be zero"
         else if size < 0 then
-            Err "slice length cannot be negative"
+            Err <| "slice length cannot be negative"
         else
-            Ok (sublists intList)
+            Ok <| sublists intList
