8148d856e396f2a72efd8c874a855fa56e4db329
Zero Delta <zerodeltahedge@gmail.com>
2022-02-09 14:14:10 +0200

    Update Tests.elm

--- accumulate/tests/Tests.elm
+++ accumulate/tests/Tests.elm
@@ -16,17 +16,22 @@ tests =
     describe "Accumulate"
         [ test "[] Accumulate" <|
             \() -> Expect.equal [] (accumulate square [])
-        ,-- skip <|
+        , skip <|
             test "square Accumulate" <|
                 \() -> Expect.equal [ 1, 4, 9 ] (accumulate square [ 1, 2, 3 ])
-        ,-- skip <|
+        , skip <|
             test "toUpper Accumulate" <|
                 \() ->
                     Expect.equal [ "HELLO", "WORLD" ]
                         (accumulate String.toUpper [ "hello", "world" ])
-        ,-- skip <|
+        , skip <|
             test "reverse Accumulate" <|
                 \() ->
                     Expect.equal [ "olleh", "dlrow" ]
                         (accumulate String.reverse [ "hello", "world" ])
+        , skip <|
+            test "accumulate recursively" <|
+                \() ->
+                    Expect.equal [ [ "a1", "a2", "a3" ], [ "b1", "b2", "b3" ], [ "c1", "c2", "c3" ] ]
+                        (accumulate (\a -> accumulate ((++) a) [ "1", "2", "3" ]) [ "a", "b", "c" ])
         ]
