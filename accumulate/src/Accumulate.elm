af562b8c203d77898248e61fef9352ef5d2c26b5
Zero Delta <zerodeltahedge@gmail.com>
2022-02-09 14:15:08 +0200

    Update Accumulate.elm

--- accumulate/src/Accumulate.elm
+++ accumulate/src/Accumulate.elm
@@ -2,8 +2,5 @@ module Accumulate exposing (accumulate)
 
 
 accumulate : (a -> b) -> List a -> List b
-accumulate func input = 
-    case input of   
-        [] -> []
-        x :: xs -> 
-            func x :: accumulate func xs
+accumulate func input =
+    Debug.todo "Please implement this function"
