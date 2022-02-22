38b2dbf881eadb787097a265e690e3b1e53ebb07
Zero Delta <zerodeltahedge@gmail.com>
2022-02-22 10:17:42 +0200

    Refactor sublist

--- sublist/src/Sublist.elm
+++ sublist/src/Sublist.elm
@@ -1,22 +1,27 @@
 module Sublist exposing (ListComparison(..), sublist)
 
+
 import List exposing(foldr)
 import String exposing(contains)
 
-type ListComparison = Equal
-                      | Superlist
-                      | Sublist
-                      | Unequal
+
+type ListComparison 
+    = Equal
+    | Superlist
+    | Sublist
+    | Unequal
 
 
 sublist : List a -> List a -> ListComparison
 sublist alist blist = 
     let 
-        stringify = foldr (Debug.toString >> (++)) ""
-        xs = alist |> stringify
-        ys = blist |> stringify
+        partOf xs ys = 
+            List.range 0 (List.length ys - List.length xs)
+            |> List.any (\x -> 
+                         List.map2 (==) (List.drop x ys) xs
+                         |> List.all (\y -> y))
     in 
-        case ((contains xs ys), (contains ys xs)) of
+        case ((partOf alist blist), (partOf blist alist)) of
             (True, True)   -> Equal
             (True, False)  -> Sublist
             (False, True)  -> Superlist
