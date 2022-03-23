7b260b13082d87e19ffbabb0ec7ca0d41ca9fa64
Zero Delta <zerodeltahedge@gmail.com>
2022-03-23 02:48:00 +0200

    Update LuigisLusciousLasagna.elm

--- luigis-luscious-lasagna/src/LuigisLusciousLasagna.elm
+++ luigis-luscious-lasagna/src/LuigisLusciousLasagna.elm
@@ -1,3 +1,14 @@
 module LuigisLusciousLasagna exposing (remainingTimeInMinutes)
 
--- TODO: define the remainingTimeInMinutes function
+
+remainingTimeInMinutes : Int -> Int -> Int
+remainingTimeInMinutes numberOfLayers minutesAlreadyInOven =
+    let
+        standardCookingTime : Int
+        standardCookingTime = 40
+
+        preparationTime : Int -> Int
+        preparationTime layers = 2 * layers
+    in
+        preparationTime numberOfLayers +
+        standardCookingTime - minutesAlreadyInOven
