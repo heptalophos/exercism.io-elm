052c361297383c79fff50b90dcc61eac78d0bc3a
Zero Delta <zerodeltahedge@gmail.com>
2022-04-04 23:45:15 +0300

    Update AnnalynsInfiltration.elm

--- annalyns-infiltration/src/AnnalynsInfiltration.elm
+++ annalyns-infiltration/src/AnnalynsInfiltration.elm
@@ -1,45 +1,46 @@
-module AnnalynsInfiltration exposing (canFastAttack, 
-                                      canFreePrisoner, 
-                                      canSignalPrisoner, 
-                                      canSpy, 
-                                      stealthAttackDamage)
+module AnnalynsInfiltration exposing 
+    ( 
+      canFastAttack, 
+      canFreePrisoner, 
+      canSignalPrisoner, 
+      canSpy, 
+      stealthAttackDamage 
+    )
 
 
 canFastAttack : Bool -> Bool
 canFastAttack knightIsAwake =
     case knightIsAwake of
-        True -> False
+        True  -> False
         False -> True
 
 
 canSpy : Bool -> Bool -> Bool -> Bool
 canSpy knightIsAwake archerIsAwake prisonerIsAwake =
     case (knightIsAwake, archerIsAwake, prisonerIsAwake) of
-        (True, _, _) -> True
-        (_, True, _) -> True
-        (_, _, True) -> True
-        (_, _, _)    -> False
-
+         (True, _, _) -> True
+         (_, True, _) -> True
+         (_, _, True) -> True
+         (_, _, _)    -> False
 
 
 canSignalPrisoner : Bool -> Bool -> Bool
 canSignalPrisoner archerIsAwake prisonerIsAwake =
     case (archerIsAwake, prisonerIsAwake) of
-        (False, True) -> True
-        (_, _) -> False
+         (False, True) -> True
+         (_, _)        -> False
 
 
 canFreePrisoner : Bool -> Bool -> Bool -> Bool -> Bool
 canFreePrisoner knightIsAwake archerIsAwake prisonerIsAwake petDogIsPresent =
     case (knightIsAwake, archerIsAwake, prisonerIsAwake) of
-        (False, False, True) -> True
-        (_, False, _) -> petDogIsPresent
-        (_, _, _) -> False
-
+         (False, False, True) -> True
+         (_, False, _)        -> petDogIsPresent
+         (_, _, _)            -> False
         
 
 stealthAttackDamage : Bool -> Int
 stealthAttackDamage annalynIsDetected =
     case annalynIsDetected of
-        True -> 7
+        True  -> 7
         False -> 12
