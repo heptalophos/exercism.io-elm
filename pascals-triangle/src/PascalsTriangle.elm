a10640e547b833812ad15329f940d3b963319733
Zero Delta <zerodeltahedge@gmail.com>
2022-07-13 20:13:37 +0300

    Update PascalsTriangle.elm

--- pascals-triangle/src/PascalsTriangle.elm
+++ pascals-triangle/src/PascalsTriangle.elm
@@ -1,36 +1,33 @@
 module PascalsTriangle exposing (rows)
-import List exposing (map)
+
+import List exposing (map, range)
 
 
 rows : Int -> List (List (Int))
 rows n =
-    List.range 1 n
-        |> List.map row
-    -- flip List.take triangle n
+    let
+        row x = 
+            range 0 x |> map (binomial x)
+    in
+        range 0 (n - 1) |> map row
+    let
+        triangle =
+            [map (binomial (x - 1)) range 0 (x - 1) | x <- range 1]   
+    in
+        flip List.take triangle n
+    -- for a triangle lazy Seq which in haskell is very easy 
+    -- mapping binomial to the range using a list comprehension
+    -- [map (binomial (x - 1)) [0..(x - 1)] | x <- [1..]]
 
 
-row : Int -> List Int
-row n = 
-    List.range 0 n |> List.map (binomial n)
+-- row : Int -> List Int
+-- row n = 
+--     range 0 n |> map (binomial n)
 
 
 binomial : Int -> Int -> Int
-binomial n k = 
+binomial n k =
     case (n, k) of
-        (_, 0) -> 
-            1
-        (0, _) -> 
-            0 
-        (_, _) -> 
-            (binomial (n - 1) (k - 1)) * (n // k) 
-
-
--- triangle : List (List (Int ))
--- triangle = 
-    -- [map (\y -> binomial (y - 1) (List.range 0 (y - 1))]) []
-    -- (List.range 1 map(\y -> (map(\x -> binomial (x - 1) y) (List.range 0 (y - 1)))))
-
-
-flip : (a -> b -> c) -> b -> a -> c
-flip f y x = 
-        f x y
+        (_, 0) -> 1
+        (0, _) -> 0 
+        (_, _) -> (binomial (n - 1) (k - 1)) * n // k 
