fb92ce9a16f30c1bcd0ba714a84c96ab6c433ddb
Zero Delta <zerodeltahedge@gmail.com>
2022-07-13 20:08:56 +0300

    Update Tests.elm

--- pascals-triangle/tests/Tests.elm
+++ pascals-triangle/tests/Tests.elm
@@ -1,8 +1,8 @@
 module Tests exposing (tests)
 
 import Expect
+import PascalsTriangle exposing (rows)
 import Test exposing (..)
-import Triangle exposing (rows)
 
 
 tests : Test
@@ -11,23 +11,23 @@ tests =
         [ test "no rows" <|
             \() ->
                 Expect.equal [] (rows 0)
-        ,-- skip <|
+        , skip <|
             test "single row" <|
                 \() ->
                     Expect.equal [ [ 1 ] ] (rows 1)
-        ,-- skip <|
+        , skip <|
             test "two rows" <|
                 \() ->
                     Expect.equal [ [ 1 ], [ 1, 1 ] ] (rows 2)
-        ,-- skip <|
+        , skip <|
             test "three rows" <|
                 \() ->
                     Expect.equal [ [ 1 ], [ 1, 1 ], [ 1, 2, 1 ] ] (rows 3)
-        ,-- skip <|
+        , skip <|
             test "four rows" <|
                 \() ->
                     Expect.equal [ [ 1 ], [ 1, 1 ], [ 1, 2, 1 ], [ 1, 3, 3, 1 ] ] (rows 4)
-        ,-- skip <|
+        , skip <|
             test "negative rows" <|
                 \() ->
                     Expect.equal [] (rows -1)
