ebc48b402d2d0ec5176b99e3b406852fcbf4d7d3
Zero Delta <zerodeltahedge@gmail.com>
2022-07-15 23:24:30 +0300

    Update Grains.elm

--- grains/src/Grains.elm
+++ grains/src/Grains.elm
@@ -3,5 +3,7 @@ module Grains exposing (square)
 
 square : Int -> Maybe Int
 square n =
-    if n > 0 then Just (2 ^ (n - 1))
-    else Nothing
+   if n > 0 then 
+      Just (2 ^ (n - 1))
+   else 
+      Nothing
