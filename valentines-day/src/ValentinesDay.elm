ef3fc6be23ffc70437bf99749b8e1268a282c4fc
Zero Delta <zerodeltahedge@gmail.com>
2022-08-29 13:37:59 +0300

    Update ValentinesDay.elm

--- valentines-day/src/ValentinesDay.elm
+++ valentines-day/src/ValentinesDay.elm
@@ -1,37 +1,5 @@
 module ValentinesDay exposing (..)
 
 
-type Approval = Yes 
-                | No 
-                | Maybe
-
-
-type Genre = Crime 
-             | Horror 
-             | Romance 
-             | Thriller
-
-
-type Cuisine = Korean 
-               | Turkish
-
-
-type Activity = BoardGame 
-                | Chill 
-                | Movie Genre 
-                | Restaurant Cuisine
-
-
-rateActivity : Activity -> Approval
 rateActivity activity =
-    case activity of 
-        Movie Romance -> 
-            Yes
-        Movie _ ->
-            No
-        Restaurant Korean -> 
-            Yes
-        Restaurant Turkish -> 
-            Maybe
-        _ -> 
-            No
+    Debug.todo "implement this function and create a type annotation"
