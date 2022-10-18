d1acca6cbaf066fad1a714a07d4d0219967350b2
Zero Delta <zerodeltahedge@gmail.com>
2022-10-18 18:42:05 +0300

    Create MariosMarvellousLasagna.elm

---
+++ marios-marvellous-lasagna/src/MariosMarvellousLasagna.elm
@@ -0,0 +1,16 @@
+module MariosMarvellousLasagna exposing (remainingTimeInMinutes)
+
+
+remainingTimeInMinutes : Int -> Int -> Int
+remainingTimeInMinutes numberOfLayers minutesAlreadyInOven =
+    let
+        standardCookingTime : Int
+        standardCookingTime = 40
+
+        preparation : Int -> Int
+        preparation = (*) 2
+
+        cooking : Int
+        cooking = (-) standardCookingTime minutesAlreadyInOven
+    in
+        numberOfLayers |> preparation |> (+) cooking
