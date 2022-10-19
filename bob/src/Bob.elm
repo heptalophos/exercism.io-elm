911835f194afbb0a76434650784a60038f3ba1c1
Zero Delta <zerodeltahedge@gmail.com>
2022-10-19 21:14:26 +0300

    Update Bob.elm

--- bob/src/Bob.elm
+++ bob/src/Bob.elm
@@ -1,38 +1,6 @@
 module Bob exposing (hey)
 
-import Char exposing (isLower, isUpper)
-import String exposing (all, 
-                        endsWith, 
-                        filter, 
-                        isEmpty, 
-                        trim)
 
 hey : String -> String
-hey remark = 
-    if (shouting remark && question remark) then
-        "Calm down, I know what I'm doing!"
-    else if shouting remark then
-        "Whoa, chill out!"
-    else if question remark then 
-        "Sure."
-    else if silence remark then
-        "Fine. Be that way!"
-    else
-        "Whatever."
-
-silence : String -> Bool
-silence = 
-    isEmpty << trim
-
-shouting : String -> Bool
-shouting msg = 
-    all isUpper (filter isAlpha msg) && 
-    not (isEmpty (filter isAlpha msg)) 
-
-question : String -> Bool
-question = 
-    trim >> endsWith "?"
-
-isAlpha : Char -> Bool
-isAlpha c = 
-    isLower (c) || isUpper (c)
+hey remark =
+    Debug.todo "Please implement this function"
