9c85cdbdc23ab62f7a407f902adabe1d9e6d9bd5
Zero Delta <zerodeltahedge@gmail.com>
2022-10-24 13:56:46 +0300

    Reformat Hamming.elm

--- hamming/src/Hamming.elm
+++ hamming/src/Hamming.elm
@@ -1,18 +1,20 @@
-module Hamming exposing (distance)
+module Hamming exposing ( distance )
 
 import Result
-import String exposing (length, toList)
-import List exposing (map2, foldl)
+import String exposing ( length, toList )
+import List exposing ( map2, foldl )
 
 
 distance : String -> String -> Result String Int
 distance left right =
     if length left /= length right then
-        Err "left and right strands must be of equal length"
+        Err "strands must be of equal length"
     else
         left 
         |> toList
-        |> (toList >> map2 (\x y -> 
-                        if x == y then 0 else 1)) right
+        |> (toList 
+            >> map2 (\x y -> 
+                        if x == y then 0 else 1)) 
+            right
         |> foldl (\x sum -> sum + x) 0
         |> Ok   
