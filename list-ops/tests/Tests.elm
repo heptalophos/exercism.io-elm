b4af5e69a6a008961f553c2be7b684841a2880d1
Zero Delta <zerodeltahedge@gmail.com>
2022-10-25 22:50:05 +0300

    Update Tests.elm

--- list-ops/tests/Tests.elm
+++ list-ops/tests/Tests.elm
@@ -11,75 +11,75 @@ tests =
         [ describe "length"
             [ test "empty list" <|
                 \() -> Expect.equal 0 (ListOps.length [])
-            , skip <|
+            ,-- skip <|
                 test "non-empty list" <|
                     \() -> Expect.equal 4 (ListOps.length (List.range 1 4))
             ]
         , describe "reverse"
-            [ skip <|
+            [-- skip <|
                 test "empty list" <|
                     \() -> Expect.equal [] (ListOps.reverse [])
-            , skip <|
+            ,-- skip <|
                 test "non-empty list" <|
                     \() -> Expect.equal [ 4, 3, 2, 1 ] (ListOps.reverse (List.range 1 4))
             ]
         , describe "map"
-            [ skip <|
+            [-- skip <|
                 test "empty list" <|
                     \() -> Expect.equal [] (ListOps.map ((+) 1) [])
-            , skip <|
+            ,-- skip <|
                 test "non-empty list" <|
                     \() -> Expect.equal (List.range 2 5) (ListOps.map ((+) 1) (List.range 1 4))
             ]
         , describe "filter"
-            [ skip <|
+            [-- skip <|
                 test "empty list" <|
                     \() -> Expect.equal [] (ListOps.filter (\_ -> True) [])
-            , skip <|
+            ,-- skip <|
                 test "non-empty list" <|
                     \() -> Expect.equal [ 2, 4 ] (ListOps.filter (\x -> modBy 2 x == 0) (List.range 1 4))
             ]
         , describe "foldl"
-            [ skip <|
+            [-- skip <|
                 test "empty list" <|
                     \() -> Expect.equal 0 (ListOps.foldl (+) 0 [])
-            , skip <|
+            ,-- skip <|
                 test "non-empty list" <|
                     \() -> Expect.equal 10 (ListOps.foldl (+) 0 (List.range 1 4))
-            , skip <|
+            ,-- skip <|
                 test "direction" <|
                     \() -> Expect.equal [ 4, 3, 2, 1 ] (ListOps.foldl (::) [] (List.range 1 4))
             ]
         , describe "foldr"
-            [ skip <|
+            [-- skip <|
                 test "empty list" <|
                     \() -> Expect.equal 0 (ListOps.foldr (+) 0 [])
-            , skip <|
+            ,-- skip <|
                 test "non-empty list" <|
                     \() -> Expect.equal 10 (ListOps.foldr (+) 0 (List.range 1 4))
-            , skip <|
+            ,-- skip <|
                 test "direction" <|
                     \() -> Expect.equal (List.range 1 4) (ListOps.foldr (::) [] (List.range 1 4))
             ]
         , describe "append"
-            [ skip <|
+            [-- skip <|
                 test "empty lists" <|
                     \() -> Expect.equal [] (ListOps.append [] [])
-            , skip <|
+            ,-- skip <|
                 test "empty and non-empty lists" <|
                     \() -> Expect.equal (List.range 1 4) (ListOps.append [] (List.range 1 4))
-            , skip <|
+            ,-- skip <|
                 test "non-empty and empty lists" <|
                     \() -> Expect.equal (List.range 1 4) (ListOps.append (List.range 1 4) [])
-            , skip <|
+            ,-- skip <|
                 test "non-empty lists" <|
                     \() -> Expect.equal (List.range 1 8) (ListOps.append (List.range 1 4) (List.range 5 8))
             ]
         , describe "concat"
-            [ skip <|
+            [-- skip <|
                 test "empty list" <|
                     \() -> Expect.equal [] (ListOps.concat [])
-            , skip <|
+            ,-- skip <|
                 test "list of lists" <|
                     \() -> Expect.equal (List.range 1 10) (ListOps.concat [ List.range 1 3, [], List.range 4 7, List.range 8 10 ])
             ]
