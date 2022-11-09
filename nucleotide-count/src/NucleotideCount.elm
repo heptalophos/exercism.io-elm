0b5755bb9d05f9721b009f61844473a4fe668aa2
Zero Delta <zerodeltahedge@gmail.com>
2022-11-09 18:06:56 +0200

    Refactor nucleotide-count

--- nucleotide-count/src/NucleotideCount.elm
+++ nucleotide-count/src/NucleotideCount.elm
@@ -1,6 +1,5 @@
 module NucleotideCount exposing (nucleotideCounts)
 
-
 type alias NucleotideCounts =
     { a : Int
     , t : Int
@@ -8,21 +7,19 @@ type alias NucleotideCounts =
     , g : Int
     }
 
-
 nucleotideCounts : String -> Result String NucleotideCounts
 nucleotideCounts sequence =
     let 
-        validStrand = 
-            sequence 
-            |> String.toList
-            |> List.all (\n -> List.member n ("ATCG" |> String.toList)) 
+        validStrand = sequence 
+                      |> String.toList
+                      |> List.all (
+                            \n -> List.member n ("ATCG" |> String.toList)
+                         ) 
     in 
         case validStrand of 
-            True -> 
-                Ok  { a = String.indices "A" sequence |> List.length
-                    , t = String.indices "T" sequence |> List.length
-                    , c = String.indices "C" sequence |> List.length
-                    , g = String.indices "G" sequence |> List.length
-                    }
-            _    -> 
-                Err "Invalid nucleotide in strand" 
+            True -> Ok  { a = String.indices "A" sequence |> List.length
+                        , t = String.indices "T" sequence |> List.length
+                        , c = String.indices "C" sequence |> List.length
+                        , g = String.indices "G" sequence |> List.length
+                        }
+            _    -> Err "Invalid nucleotide in strand" 
