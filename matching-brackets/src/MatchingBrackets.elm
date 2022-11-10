13a89165f99045ab6f9ba9eadfd9210dcd470fad
Zero Delta <zerodeltahedge@gmail.com>
2022-11-10 23:43:56 +0200

    Update MatchingBrackets.elm

--- matching-brackets/src/MatchingBrackets.elm
+++ matching-brackets/src/MatchingBrackets.elm
@@ -5,35 +5,23 @@ import String exposing (contains, fromChar, toList)
 
 isPaired : String -> Bool
 isPaired input =
-    paired (toList input) []
+    paired (input |> toList) []
 
 
 paired : List Char -> List Char -> Bool
 paired s1 s2 = 
     case (s1, s2) of
-        ([], []) -> 
-            True
-        ([], _) -> 
-            False
-        ((']'::xs), ('['::ys)) -> 
-            paired xs ys
-        ((')'::xs), ('('::ys)) -> 
-            paired xs ys
-        (('}'::xs), ('{'::ys)) -> 
-            paired xs ys
+        ([], []) -> True
+        ([], _)  -> False
+        ((']'::xs), ('['::ys)) -> paired xs ys
+        ((')'::xs), ('('::ys)) -> paired xs ys
+        (('}'::xs), ('{'::ys)) -> paired xs ys
         ((x::xs), stack) -> 
             case x of
-                '}' -> 
-                    False
-                ')' -> 
-                    False
-                ']' -> 
-                    False
-                '[' -> 
-                    paired xs (x::stack)
-                '{' -> 
-                    paired xs (x::stack)
-                '(' -> 
-                    paired xs (x::stack)
-                _   -> 
-                    paired xs stack
+                '}' -> False
+                ')' -> False
+                ']' -> False
+                '[' -> paired xs (x::stack)
+                '{' -> paired xs (x::stack)
+                '(' -> paired xs (x::stack)
+                _   -> paired xs stack
