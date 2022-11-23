2e2683e666ee486167ac1e7f601a1e445e226c0e
Zero Delta <zerodeltahedge@gmail.com>
2022-11-23 22:58:18 +0200

    Create TisburyTreasureHunt.elm

---
+++ tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
@@ -0,0 +1,29 @@
+module TisburyTreasureHunt exposing (..)
+
+-- Consider defining a type alias for TreasureLocation,
+-- Treasure, PlaceLocation and Place,
+-- and using them in the function type annotations
+-- type alias TreasureLocation = ( Int, Char )
+-- type alias PlaceLocation = ( Char, Int )
+-- type alias Treasure = ( String, TreasureLocation )
+-- type alias Place = ( String, PlaceLocation )
+
+
+placeLocationToTreasureLocation : ( Char, Int ) -> ( Int, Char )
+placeLocationToTreasureLocation placeLocation =
+    Debug.todo "implement this function"
+
+
+treasureLocationMatchesPlaceLocation : ( Char, Int ) -> ( Int, Char ) -> Bool
+treasureLocationMatchesPlaceLocation placeLocation treasureLocation =
+    Debug.todo "implement this function"
+
+
+countPlaceTreasures : ( String, ( Char, Int ) ) -> List ( String, ( Int, Char ) ) -> Int
+countPlaceTreasures place treasures =
+    Debug.todo "implement this function"
+
+
+specialCaseSwapPossible : ( String, TreasureLocation ) -> ( String, PlaceLocation ) -> ( String, TreasureLocation ) -> Bool
+specialCaseSwapPossible ( foundTreasure, _ ) ( place, _ ) ( desiredTreasure, _ ) =
+    Debug.todo "implement this function"
