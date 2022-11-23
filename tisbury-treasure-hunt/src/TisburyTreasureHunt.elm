4ae9d11b8c334004a14666b85b1f031d2aaaaf4b
Zero Delta <zerodeltahedge@gmail.com>
2022-11-23 23:01:24 +0200

    TisburyTreasureHunt.elm - 1st iteration, passing all tests

--- tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
+++ tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
@@ -3,27 +3,41 @@ module TisburyTreasureHunt exposing (..)
 -- Consider defining a type alias for TreasureLocation,
 -- Treasure, PlaceLocation and Place,
 -- and using them in the function type annotations
--- type alias TreasureLocation = ( Int, Char )
--- type alias PlaceLocation = ( Char, Int )
--- type alias Treasure = ( String, TreasureLocation )
--- type alias Place = ( String, PlaceLocation )
+type alias TreasureLocation = ( Int, Char )
+type alias PlaceLocation = ( Char, Int )
+type alias Treasure = ( String, TreasureLocation )
+type alias Place = ( String, PlaceLocation )
 
 
-placeLocationToTreasureLocation : ( Char, Int ) -> ( Int, Char )
+placeLocationToTreasureLocation : PlaceLocation -> TreasureLocation
 placeLocationToTreasureLocation placeLocation =
-    Debug.todo "implement this function"
+    let
+        (longitude, latitude) = placeLocation
+    in 
+        (latitude, longitude)
 
 
 treasureLocationMatchesPlaceLocation : ( Char, Int ) -> ( Int, Char ) -> Bool
 treasureLocationMatchesPlaceLocation placeLocation treasureLocation =
-    Debug.todo "implement this function"
+    (placeLocation |> placeLocationToTreasureLocation) == treasureLocation
 
 
-countPlaceTreasures : ( String, ( Char, Int ) ) -> List ( String, ( Int, Char ) ) -> Int
+countPlaceTreasures : Place -> List Treasure -> Int
 countPlaceTreasures place treasures =
-    Debug.todo "implement this function"
+    let 
+        (_, location) = place
+    in
+        treasures |> List.map (\(_, treasureLocation) -> treasureLocation)
+                  |> List.filter (location |> treasureLocationMatchesPlaceLocation)
+                  |> List.length
 
 
 specialCaseSwapPossible : ( String, TreasureLocation ) -> ( String, PlaceLocation ) -> ( String, TreasureLocation ) -> Bool
 specialCaseSwapPossible ( foundTreasure, _ ) ( place, _ ) ( desiredTreasure, _ ) =
-    Debug.todo "implement this function"
+    case (foundTreasure, place, desiredTreasure) of
+        ("Brass Spyglass", "Abandoned Lighthouse", _) -> True
+        ("Amethyst Octopus", "Stormy Breakwater", "Crystal Crab") -> True
+        ("Amethyst Octopus", "Stormy Breakwater", "Glass Starfish") -> True
+        ("Vintage Pirate Hat", "Harbor Managers Office", "Model Ship in Large Bottle") -> True
+        ("Vintage Pirate Hat", "Harbor Managers Office", "Antique Glass Fishnet Float") -> True
+        _ -> False
