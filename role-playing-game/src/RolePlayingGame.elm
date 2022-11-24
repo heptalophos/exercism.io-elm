44f2c4513b9998fa2baca0ccf5fa1a6b5b6b0066
Zero Delta <zerodeltahedge@gmail.com>
2022-11-24 14:52:37 +0200

    Update RolePlayingGame.elm

--- role-playing-game/src/RolePlayingGame.elm
+++ role-playing-game/src/RolePlayingGame.elm
@@ -11,14 +11,49 @@ type alias Player =
 
 introduce : Player -> String
 introduce { name } =
-    Debug.todo "Please implement this function"
+    name |> Maybe.withDefault "Mighty Magician" 
 
 
 revive : Player -> Maybe Player
 revive player =
-    Debug.todo "Please implement this function"
+    if player.health == 0 then
+        case player.level > 9 of
+            True -> 
+                Just { name = player.name
+                     , level = player.level
+                     , health = 100
+                     , mana = Just 100 
+                     }
+            _ -> 
+                Just { name = player.name
+                     , level = player.level
+                     , health = 100
+                     , mana = Nothing 
+                     }
+    else
+        Nothing
 
 
 castSpell : Int -> Player -> ( Player, Int )
 castSpell manaCost player =
-    Debug.todo "Please implement this function"
+    case Maybe.map ((<=) manaCost) player.mana of
+        Just True -> 
+            ( { name = player.name
+              , level = player.level
+              , health = player.health
+              , mana = player.mana 
+                       |> Maybe.map (((+) -manaCost) >> max 0) 
+              }
+            , (*) 2 manaCost
+            )
+        Just _ -> 
+            ( player, 0 )
+        Nothing ->
+            ( { name = player.name
+              , level =player.level
+              , health = player.health - manaCost |> max 0 
+              , mana = player.mana
+              }
+            , 
+              0
+            )
