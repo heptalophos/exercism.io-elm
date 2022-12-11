9e6eef3e04bfc380781c7059def107e2e6e66c7b
Zero Delta <zerodeltahedge@gmail.com>
2022-12-12 01:11:42 +0200

    Reformat AtbashCipher.elm

--- atbash-cipher/src/AtbashCipher.elm
+++ atbash-cipher/src/AtbashCipher.elm
@@ -13,24 +13,21 @@ chunkify n text =
 atbash : Char -> Char 
 atbash ch =
     if Char.isAlpha ch then
-        Char.fromCode((Char.toCode 'a') + 
-        (Char.toCode 'z') - Char.toCode ch)
+        Char.fromCode((Char.toCode 'a') + (Char.toCode 'z') - Char.toCode ch)
     else 
         ch
 
 
 encode : String -> String
 encode plain = 
-    plain 
-    |> String.toLower
-    |> String.filter (\c -> Char.isLower c || Char.isDigit c)
-    |> String.map atbash
-    |> chunkify 5
-    |> String.trim
+    plain |> String.toLower
+          |> String.filter (\c -> Char.isLower c || Char.isDigit c)
+          |> String.map atbash
+          |> chunkify 5
+          |> String.trim
 
 
 decode : String -> String
 decode cipher = 
-    cipher
-    |> String.filter (\c -> Char.isLower c || Char.isDigit c)
-    |> String.map atbash
+    cipher |> String.filter (\c -> Char.isLower c || Char.isDigit c)
+           |> String.map atbash
