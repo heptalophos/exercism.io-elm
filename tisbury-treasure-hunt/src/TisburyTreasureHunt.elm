bb48ba4fbe1121eabf80dfbbcc67741e60a8b620
Zero Delta <zerodeltahedge@gmail.com>
2025-02-18 15:59:25 +0200

    Update TisburyTreasureHunt.elm

--- tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
+++ tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
@@ -3,48 +3,23 @@ module TisburyTreasureHunt exposing (..)
 -- Consider defining a type alias for TreasureLocation,
 -- Treasure, PlaceLocation and Place,
 -- and using them in the function type annotations
-type alias TreasureLocation = ( Int, Char )
-type alias PlaceLocation = ( Char, Int )
-type alias Treasure = ( String, TreasureLocation )
-type alias Place = ( String, PlaceLocation )
 
 
-placeLocationToTreasureLocation : PlaceLocation -> TreasureLocation
+placeLocationToTreasureLocation : ( Char, Int ) -> ( Int, Char )
 placeLocationToTreasureLocation placeLocation =
-    let
-        (char, int) = placeLocation
-    in 
-        (int, char)
+    Debug.todo "implement this function"
 
 
 treasureLocationMatchesPlaceLocation : ( Char, Int ) -> ( Int, Char ) -> Bool
 treasureLocationMatchesPlaceLocation placeLocation treasureLocation =
-   placeLocation |> placeLocationToTreasureLocation
-                 |> (==) treasureLocation
+    Debug.todo "implement this function"
 
 
-countPlaceTreasures : Place -> List Treasure -> Int
+countPlaceTreasures : ( String, ( Char, Int ) ) -> List ( String, ( Int, Char ) ) -> Int
 countPlaceTreasures place treasures =
-    let 
-        (_, pLoc) = place
-    in
-        treasures |> List.map (\(_, tLoc) -> tLoc)
-                  |> List.filter (pLoc |> treasureLocationMatchesPlaceLocation)
-                  |> List.length
+    Debug.todo "implement this function"
 
 
 specialCaseSwapPossible : ( String, TreasureLocation ) -> ( String, PlaceLocation ) -> ( String, TreasureLocation ) -> Bool
 specialCaseSwapPossible ( foundTreasure, _ ) ( place, _ ) ( desiredTreasure, _ ) =
-    case (foundTreasure, place, desiredTreasure) of
-        ("Brass Spyglass", "Abandoned Lighthouse", _) -> 
-            True
-        ("Amethyst Octopus", "Stormy Breakwater", "Crystal Crab") -> 
-            True
-        ("Amethyst Octopus", "Stormy Breakwater", "Glass Starfish") -> 
-            True
-        ("Vintage Pirate Hat", "Harbor Managers Office", "Model Ship in Large Bottle") -> 
-            True
-        ("Vintage Pirate Hat", "Harbor Managers Office", "Antique Glass Fishnet Float") -> 
-            True
-        (_, _, _) -> 
-            False
+    Debug.todo "implement this function"
