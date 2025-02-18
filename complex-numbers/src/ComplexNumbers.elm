fa86fd6fc90cf12f301668a6d697dc577decb7c9
Zero Delta <zerodeltahedge@gmail.com>
2025-02-18 19:17:43 +0200

    Create ComplexNumbers

--- complex-numbers/src/ComplexNumbers.elm
+++ complex-numbers/src/ComplexNumbers.elm
@@ -13,61 +13,79 @@ module ComplexNumbers exposing
     , sub
     )
 
-
-type Complex
-    = Todo
-
+type Complex = Complex Float Float
 
 fromPair : ( Float, Float ) -> Complex
 fromPair pair =
-    Debug.todo "Please implement fromPair."
-
+    let (re, im) = pair in Complex re im
 
 fromReal : Float -> Complex
-fromReal real =
-    Debug.todo "Please implement fromReal."
-
+fromReal re = 
+    Complex re 0.0
 
 real : Complex -> Float
 real z =
-    Debug.todo "Please implement real."
-
+    let (Complex re _) = z in re
+    
 
 imaginary : Complex -> Float
 imaginary z =
-    Debug.todo "Please implement imaginary."
-
+    let (Complex _ im) = z in im
 
 conjugate : Complex -> Complex
-conjugate z =
-    Debug.todo "Please implement conjugate."
-
-
+conjugate z = 
+    let (Complex re im) = z in Complex re -im
+    
 abs : Complex -> Float
-abs z =
-    Debug.todo "Please implement abs."
-
+abs z = 
+    let (Complex re im) = z in (re ^ 2 + im ^ 2) ^ 0.5    
 
 add : Complex -> Complex -> Complex
 add z1 z2 =
-    Debug.todo "Please implement add."
-
+    let
+        (Complex r1 i1) = z1
+        (Complex r2 i2) = z2
+    in
+        Complex (r1 + r2) (i1 + i2)
+           
 
 sub : Complex -> Complex -> Complex
 sub z1 z2 =
-    Debug.todo "Please implement sub."
+    let
+        (Complex r1 i1) = z1
+        (Complex r2 i2) = z2
+    in
+        add (Complex r1 i1) (Complex -r2 -i2)
 
 
 mul : Complex -> Complex -> Complex
 mul z1 z2 =
-    Debug.todo "Please implement mul."
+    let
+        (Complex r1 i1) = z1
+        (Complex r2 i2) = z2
+    in
+        Complex (r1 * r2 - i1 * i2) (r1 * i2 + r2 * i1)
 
 
 div : Complex -> Complex -> Complex
 div z1 z2 =
-    Debug.todo "Please implement div."
+    let
+        reducedConjugate z =
+            let
+                d = (z |> abs) ^ 2
+                (Complex r i) = z
+            in
+                Complex (r / d) -(i / d)
+    in
+        mul z1 (reducedConjugate z2)
+
+    
 
 
 exp : Complex -> Complex
 exp z =
-    Debug.todo "Please implement exp."
+    
+    let
+        (Complex re im) = z
+    in
+        mul (Complex (e ^ re) 0) (Complex (cos im) (sin im))   
