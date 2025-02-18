69b0e87aa2bc5118c67de3e4967d35f4b3430f9d
Zero Delta <zerodeltahedge@gmail.com>
2025-02-18 16:08:51 +0200

    Update TisburyTreasureHunt.elm

--- tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
+++ tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
@@ -1,25 +1,48 @@
 module TisburyTreasureHunt exposing (..)
 
--- Consider defining a type alias for TreasureLocation,
--- Treasure, PlaceLocation and Place,
--- and using them in the function type annotations
+type alias TreasureLocation = ( Int, Char )
+type alias PlaceLocation = ( Char, Int )
+type alias Treasure = ( String, TreasureLocation )
+type alias Place = ( String, PlaceLocation )
 
 
-placeLocationToTreasureLocation : ( Char, Int ) -> ( Int, Char )
+
+placeLocationToTreasureLocation : PlaceLocation -> TreasureLocation
 placeLocationToTreasureLocation placeLocation =
-    Debug.todo "implement this function"
+    let
+        (char, int) = placeLocation
+    in 
+        (int, char)
 
 
-treasureLocationMatchesPlaceLocation : ( Char, Int ) -> ( Int, Char ) -> Bool
+treasureLocationMatchesPlaceLocation : PlaceLocation -> TreasureLocation -> Bool
 treasureLocationMatchesPlaceLocation placeLocation treasureLocation =
-    Debug.todo "implement this function"
+   placeLocation |> placeLocationToTreasureLocation
+                 |> (==) treasureLocation
 
 
-countPlaceTreasures : ( String, ( Char, Int ) ) -> List ( String, ( Int, Char ) ) -> Int
+countPlaceTreasures : Place -> List Treasure -> Int
 countPlaceTreasures place treasures =
-    Debug.todo "implement this function"
+    let 
+        (_, pLoc) = place
+    in
+        treasures |> List.map (\(_, tLoc) -> tLoc)
+        |> List.filter (pLoc |> treasureLocationMatchesPlaceLocation)
+        |> List.length
 
 
 specialCaseSwapPossible : ( String, TreasureLocation ) -> ( String, PlaceLocation ) -> ( String, TreasureLocation ) -> Bool
 specialCaseSwapPossible ( foundTreasure, _ ) ( place, _ ) ( desiredTreasure, _ ) =
-    Debug.todo "implement this function"
+    case (foundTreasure, place, desiredTreasure) of
+        ("Brass Spyglass", "Abandoned Lighthouse", _) -> 
+            True
+        ("Amethyst Octopus", "Stormy Breakwater", "Crystal Crab") -> 
+            True
+        ("Amethyst Octopus", "Stormy Breakwater", "Glass Starfish") -> 
+            True
+        ("Vintage Pirate Hat", "Harbor Managers Office", "Model Ship in Large Bottle") -> 
+            True
+        ("Vintage Pirate Hat", "Harbor Managers Office", "Antique Glass Fishnet Float") -> 
+            True
+        (_, _, _) -> 
+            False
