d8393387da2d493a7415cbf13275c14d85135e14
Zero Delta <zerodeltahedge@gmail.com>
2025-02-26 18:33:04 +0200

    Reformat ComplexNumbers

--- complex-numbers/src/ComplexNumbers.elm
+++ complex-numbers/src/ComplexNumbers.elm
@@ -17,7 +17,10 @@ type Complex = Complex Float Float
 
 fromPair : ( Float, Float ) -> Complex
 fromPair pair =
-    let (re, im) = pair in Complex re im
+    let 
+        (re, im) = pair 
+    in 
+        Complex re im
 
 fromReal : Float -> Complex
 fromReal re = 
@@ -25,20 +28,31 @@ fromReal re =
 
 real : Complex -> Float
 real z =
-    let (Complex re _) = z in re
+    let 
+        (Complex re _) = z 
+    in 
+        re
     
-
 imaginary : Complex -> Float
 imaginary z =
-    let (Complex _ im) = z in im
+    let 
+        (Complex _ im) = z 
+    in 
+        im
 
 conjugate : Complex -> Complex
 conjugate z = 
-    let (Complex re im) = z in Complex re -im
+    let 
+        (Complex re im) = z 
+    in 
+        Complex re -im
     
 abs : Complex -> Float
 abs z = 
-    let (Complex re im) = z in (re ^ 2 + im ^ 2) ^ 0.5    
+    let 
+        (Complex re im) = z 
+    in 
+        (re ^ 2 + im ^ 2) ^ 0.5    
 
 add : Complex -> Complex -> Complex
 add z1 z2 =
@@ -48,7 +62,6 @@ add z1 z2 =
     in
         Complex (r1 + r2) (i1 + i2)
            
-
 sub : Complex -> Complex -> Complex
 sub z1 z2 =
     let
@@ -57,7 +70,6 @@ sub z1 z2 =
     in
         add (Complex r1 i1) (Complex -r2 -i2)
 
-
 mul : Complex -> Complex -> Complex
 mul z1 z2 =
     let
@@ -65,7 +77,6 @@ mul z1 z2 =
         (Complex r2 i2) = z2
     in
         Complex (r1 * r2 - i1 * i2) (r1 * i2 + r2 * i1)
-
 
 div : Complex -> Complex -> Complex
 div z1 z2 =
@@ -79,12 +90,8 @@ div z1 z2 =
     in
         mul z1 (reducedConjugate z2)
 
-    
-
-
 exp : Complex -> Complex
 exp z =
-    
     let
         (Complex re im) = z
     in
