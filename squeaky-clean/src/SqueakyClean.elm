3adf59f0dff2871fd1ec95b4edf326e324a73900
Zero Delta <zerodeltahedge@gmail.com>
2025-03-06 22:26:42 +0200

    Update SqueakyClean.elm

--- squeaky-clean/src/SqueakyClean.elm
+++ squeaky-clean/src/SqueakyClean.elm
@@ -1,26 +1,37 @@
 module SqueakyClean exposing (clean, clean1, clean2, clean3, clean4)
+import String exposing (replace, map, filter)
 
 
 clean1 : String -> String
-clean1 str =
-    Debug.todo "Please implement this function"
-
+clean1 =
+    replace " " "_"
 
 clean2 : String -> String
-clean2 str =
-    Debug.todo "Please implement this function"
-
+clean2 =
+    let
+        replaceCtrls = 
+            replace "\n" "[CTRL]" <<
+            map (\x -> if (Char.toCode x) < 32 then '\n' else x)
+    in 
+        clean1 >> replaceCtrls
 
 clean3 : String -> String
-clean3 str =
-    Debug.todo "Please implement this function"
-
+clean3 =
+    let
+        toCamel = replace "-ḃ" "Ḃ" >> replace "-a" "A"
+    in 
+        clean2 >> toCamel
 
 clean4 : String -> String
-clean4 str =
-    Debug.todo "Please implement this function"
-
+clean4 =
+    let 
+        removeDigits = filter (\c -> c |> Char.isDigit |> not)
+    in
+        clean3 >> removeDigits 
 
 clean : String -> String
-clean str =
-    Debug.todo "Please implement this function"
+clean =
+    let
+        removeGreeks = filter (\char -> char < 'α' || char > 'ω')
+    in 
+        clean4 >> removeGreeks 
