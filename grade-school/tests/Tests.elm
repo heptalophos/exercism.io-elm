23284ab1501c9e1e23cffcba32fd235b97c51a02
Zero Delta <zerodeltahedge@gmail.com>
2025-03-12 12:22:32 +0200

    Update Tests.elm

--- grade-school/tests/Tests.elm
+++ grade-school/tests/Tests.elm
@@ -21,86 +21,86 @@ tests =
     describe "GradeSchool"
         [ test "Roster is empty when no student is added" <|
             \() -> Expect.equal [] (allStudents emptySchool)
-        , skip <|
+        , -- skip <|
             test "Add a student" <|
                 \() ->
                     addStudent 2 "Aimee" emptySchool
                         |> Tuple.first
                         |> Expect.equal Added
-        , skip <|
+        , -- skip <|
             test "Student is added to the roster" <|
                 \() ->
                     addStudent 2 "Aimee" emptySchool
                         |> Tuple.second
                         |> allStudents
                         |> Expect.equal [ "Aimee" ]
-        , skip <|
+        , -- skip <|
             test "Adding multiple students in the same grade in the roster" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Blair", 2 ), ( "James", 2 ), ( "Paul", 2 ) ]
                         |> Tuple.first
                         |> Expect.equal [ Added, Added, Added ]
-        , skip <|
+        , -- skip <|
             test "Multiple students in the same grade are added to the roster" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Blair", 2 ), ( "James", 2 ), ( "Paul", 2 ) ]
                         |> Tuple.second
                         |> allStudents
                         |> Expect.equal [ "Blair", "James", "Paul" ]
-        , skip <|
+        , -- skip <|
             test "Cannot add student to same grade in the roster more than once" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Blair", 2 ), ( "James", 2 ), ( "James", 2 ), ( "Paul", 2 ) ]
                         |> Tuple.first
                         |> Expect.equal [ Added, Added, Duplicate, Added ]
-        , skip <|
+        , -- skip <|
             test "Student not added to same grade in the roster more than once" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Blair", 2 ), ( "James", 2 ), ( "James", 2 ), ( "Paul", 2 ) ]
                         |> Tuple.second
                         |> allStudents
                         |> Expect.equal [ "Blair", "James", "Paul" ]
-        , skip <|
+        , -- skip <|
             test "Adding students in multiple grades" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Chelsea", 3 ), ( "Logan", 7 ) ]
                         |> Tuple.first
                         |> Expect.equal [ Added, Added ]
-        , skip <|
+        , -- skip <|
             test "Students in multiple grades are added to the roster" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Chelsea", 3 ), ( "Logan", 7 ) ]
                         |> Tuple.second
                         |> allStudents
                         |> Expect.equal [ "Chelsea", "Logan" ]
-        , skip <|
+        , -- skip <|
             test "Cannot add same student to multiple grades in the roster" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Blair", 2 ), ( "James", 2 ), ( "James", 3 ), ( "Paul", 3 ) ]
                         |> Tuple.first
                         |> Expect.equal [ Added, Added, Duplicate, Added ]
-        , skip <|
+        , -- skip <|
             test "Student not added to multiple grades in the roster" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Blair", 2 ), ( "James", 2 ), ( "James", 3 ), ( "Paul", 3 ) ]
                         |> Tuple.second
                         |> allStudents
                         |> Expect.equal [ "Blair", "James", "Paul" ]
-        , skip <|
+        , -- skip <|
             test "Students are sorted by grades in the roster" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Jim", 3 ), ( "Peter", 2 ), ( "Anna", 1 ) ]
                         |> Tuple.second
                         |> allStudents
                         |> Expect.equal [ "Anna", "Peter", "Jim" ]
-        , skip <|
+        , -- skip <|
             test "Students are sorted by name in the roster" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Peter", 2 ), ( "Zoe", 2 ), ( "Alex", 2 ) ]
                         |> Tuple.second
                         |> allStudents
                         |> Expect.equal [ "Alex", "Peter", "Zoe" ]
-        , skip <|
+        , -- skip <|
             test "Students are sorted by grades and then by name in the roster" <|
                 \() ->
                     buildSchoolwithStudents
@@ -115,39 +115,39 @@ tests =
                         |> Tuple.second
                         |> allStudents
                         |> Expect.equal [ "Anna", "Barb", "Charlie", "Alex", "Peter", "Zoe", "Jim" ]
-        , skip <|
+        , -- skip <|
             test "Grade is empty if no students in the roster" <|
                 \() ->
                     Expect.equal [] (studentsInGrade 1 emptySchool)
-        , skip <|
+        , -- skip <|
             test "Grade is empty if no students in that grade" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Peter", 2 ), ( "Zoe", 2 ), ( "Alex", 2 ), ( "Jim", 3 ) ]
                         |> Tuple.second
                         |> studentsInGrade 1
                         |> Expect.equal []
-        , skip <|
+        , -- skip <|
             test "Student not added to same grade more than once" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Blair", 2 ), ( "James", 2 ), ( "James", 2 ), ( "Paul", 2 ) ]
                         |> Tuple.second
                         |> studentsInGrade 2
                         |> Expect.equal [ "Blair", "James", "Paul" ]
-        , skip <|
+        , -- skip <|
             test "Student not added to multiple grades" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Blair", 2 ), ( "James", 2 ), ( "James", 3 ), ( "Paul", 3 ) ]
                         |> Tuple.second
                         |> studentsInGrade 2
                         |> Expect.equal [ "Blair", "James" ]
-        , skip <|
+        , -- skip <|
             test "Student not added to other grade for multiple grades" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Blair", 2 ), ( "James", 2 ), ( "James", 3 ), ( "Paul", 3 ) ]
                         |> Tuple.second
                         |> studentsInGrade 3
                         |> Expect.equal [ "Paul" ]
-        , skip <|
+        , -- skip <|
             test "Students are sorted by name in a grade" <|
                 \() ->
                     buildSchoolwithStudents [ ( "Franklin", 5 ), ( "Bradley", 5 ), ( "Jeff", 1 ) ]
