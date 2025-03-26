328a2f211f1fa44c478cde25d0996b64b0a52988
Zero Delta <zerodeltahedge@gmail.com>
2025-03-26 21:53:59 +0200

    Update ReverseString.elm

--- reverse-string/src/ReverseString.elm
+++ reverse-string/src/ReverseString.elm
@@ -1,6 +1,7 @@
 module ReverseString exposing (reverse)
 
+import String exposing (foldl, cons)
 
 reverse : String -> String
-reverse str =
-    Debug.todo "Please implement this function"
+reverse str=
+    foldl cons "" str
